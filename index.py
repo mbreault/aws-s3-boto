9e8aa3e58dbf8d577a0e6561533f693bd8619ccd
Mike Breault <mikeb@mcrtechnologies.com>
2020-02-11 19:09:55 -0500

    initial commit

---
+++ index.py
@@ -0,0 +1,13 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def upload_file(file_name, bucket, object_name):
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
